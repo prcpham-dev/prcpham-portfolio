9c024d8e44fc89c94ed5ecccd52eddafddb7271e
erii__2 <114847917+vibecoder-prc@users.noreply.github.com>
2025-09-18 15:28:15 -0700

    Set up nextjs tailwindcss and ts

---
+++ portfolio/pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-white text-gray-900">
+      <h1 className="text-4xl font-bold">Hello, I’m prc_pham 👋</h1>
+    </main>
+  );
+};
+
+export default Home;
