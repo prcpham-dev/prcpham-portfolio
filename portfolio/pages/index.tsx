cad2d5ced26189d42628d9f821f0acb85357c89d
erii__2 <114847917+vibecoder-prc@users.noreply.github.com>
2025-09-21 20:31:32 -0700

    Forgot about index.tsx

--- portfolio/pages/index.tsx
+++ portfolio/pages/index.tsx
@@ -4,7 +4,7 @@ import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
 
 const About = dynamic(() => import("./components/about/About"), { ssr: false });
-const Intro = dynamic(() => import("./components/Intro/Intro"), { ssr: false });
+const Intro = dynamic(() => import("./components/intro/Intro"), { ssr: false });
 const Experience = dynamic(() => import("./components/experience/Experience"), { ssr: false });
 const Projects = dynamic(() => import("./components/projects/Projects"), { ssr: false });
 
