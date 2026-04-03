2f5e9d5f90b6e63520924835d34827c47eb6f355
erii__2 <percypham279@gmail.com>
2026-04-03 02:54:04 -0700

    Fix missing pdfs

--- portfolio/data/pdfs.ts
+++ portfolio/data/pdfs.ts
@@ -2,6 +2,6 @@ export const pdfs = [
     { name: 'Resume', path: 'resume.pdf', favorite: true },
     { name: 'The Blueprint', path: 'sitemap/The_Blueprint.pdf', favorite: true },
     { name: 'Milestone of Growth', path: 'sitemap/Milestones_of_Growth.pdf' },
-    { name: 'The Delivery Lady', path: 'sitemap/The_Delivery_Lady.pdf' },
+    { name: 'The Delivery Lady', path: 'sitemap/The_delivery_lady.pdf' },
     { name: 'Vivian', path: 'sitemap/Vivian.pdf', favorite: true },
 ];
