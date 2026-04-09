6da973a1c5331342bf05107ebd1d648c058ab6f4
erii__2 <percypham279@gmail.com>
2026-04-09 15:56:24 -0700

    add story

--- portfolio/data/pdfs.ts
+++ portfolio/data/pdfs.ts
@@ -1,5 +1,6 @@
 export const pdfs = [
     { name: 'Resume', slug: 'resume', path: 'sitemap/resume.pdf', favorite: true },
+    { name: 'TABLOID NEWS', slug: 'tabloid-news', path: 'sitemap/TABLOID_NEWS.pdf', favorite: true },
     { name: 'The Blueprint', slug: 'the-blueprint', path: 'sitemap/The_Blueprint.pdf', favorite: true },
     { name: 'Milestone of Growth', slug: 'milestone-of-growth', path: 'sitemap/Milestones_of_Growth.pdf' },
     { name: 'Vivian', slug: 'vivian', path: 'sitemap/Vivian.pdf', favorite: true },
