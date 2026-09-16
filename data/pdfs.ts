f205c0655d060754d321350270bc39a9fe9fbed1
erii__2 <percypham279@gmail.com>
2026-09-15 23:19:44 -0700

    Edit: Change sitemap

--- data/pdfs.ts
+++ data/pdfs.ts
@@ -3,6 +3,6 @@ export const pdfs = [
     { name: 'TABLOID NEWS', slug: 'tabloid-news', path: 'sitemap/TABLOID_NEWS.pdf', favorite: true },
     { name: 'The Blueprint', slug: 'the-blueprint', path: 'sitemap/The_Blueprint.pdf', favorite: true },
     { name: 'Milestone of Growth', slug: 'milestone-of-growth', path: 'sitemap/Milestones_of_Growth.pdf' },
-    { name: 'Vivian', slug: 'vivian', path: 'sitemap/Vivian.pdf', favorite: true },
+    { name: 'Vivian', slug: 'vivian', path: 'sitemap/vivian.pdf', favorite: true },
     { name: 'The Delivery Lady', slug: 'the-delivery-lady', path: 'sitemap/The_delivery_lady.pdf' },
 ];
